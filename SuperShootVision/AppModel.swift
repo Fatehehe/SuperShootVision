a5b7cd6d315e1ca6e4ba00fa040cb2c9d07f0d3c
Fatehehe <fatakhillah.khaqo@gmail.com>
2026-07-02 12:19:13 +0700

    Initial Commit

---
+++ SuperShootVision/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  SuperShootVision
+//
+//  Created by Fatakhillah Khaqo on 02/07/26.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
